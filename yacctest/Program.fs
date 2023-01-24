26416d33a8c86f1c11bd8a71700abe4419979f33
Hugo Simonsson <hugosi@student.chalmers.se>
2023-01-24 15:25:44 +0100

    WIP 4

--- yacctest/Program.fs
+++ yacctest/Program.fs
@@ -5,12 +5,23 @@ open FSharp.Text.Lexing
 let main argv =
     let parse json = 
         let lexbuf = LexBuffer<char>.FromString json
-        // let res = Parser.start Lexer.read lexbuf
-        lexbuf
-    let simpleJson = @"rule Testing for real: 
-      project types: 
-      ""Test test test""  
+        let res = Parser.start Lexer.read lexbuf
+        res
+    let simpleJson = @"precision 2
+      rule satt automatiskt produktionsstart:
+        project types: 
+          ""Göteborg - 1 Små projekt under 100 tkr""
+          ""Göteborg - 2 Mindre projekt mellan 100tkr -625tkr""
+          ""Göteborg - 3 Standard projekt över 625 tkr""
+        sources:
+          task 
+            when Referens = genomför 
+            get StartDate
+        transformation:
+          none
+        destination:
+          project property Produktionsstart 
     "
     let (parseResult) = simpleJson |> parse
-    printfn "%A" parseResult
-    0
+    printfn "%A" (parseResult)
+    0 
