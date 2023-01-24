4567baf941fae72b030c6fdbc7b83664e3a3373c
Lucas Glimfjord <lucas.glimfjord@antura.se>
2023-01-24 10:15:28 +0100

    commit

---
+++ yacctest/JsonValue.fs
@@ -0,0 +1,8 @@
+type JsonValue = 
+  | Assoc of (string * JsonValue) list
+  | Bool of bool
+  | Float of float
+  | Int of int
+  | List of JsonValue list
+  | Null
+  | String of string
--- yacctest/Program.fs
+++ yacctest/Program.fs
@@ -1,2 +1,20 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Program
+open FSharp.Text.Lexing
+open JsonParsing
+
+[<EntryPoint>]
+let main argv =
+    let parse json = 
+        let lexbuf = LexBuffer<char>.FromString json
+        let res = Parser.start Lexer.read lexbuf
+        res
+    let simpleJson = @"{
+              ""title"": ""Cities"",
+              ""cities"": [
+                { ""name"": ""Chicago"",  ""zips"": [60601,60600] },
+                { ""name"": ""New York"", ""zips"": [10001] } 
+              ]
+            }"
+    let (Some parseResult) = simpleJson |> parse
+
+0
