d02ec6ad22d1d169aaa9e51801a8f681f9af5430
Lucas Glimfjord <lucas.glimfjord@antura.se>
2023-02-06 10:53:15 +0100

    Parses fsy files

--- sempar/HelperFunctions.fs
+++ sempar/HelperFunctions.fs
@@ -1,3 +1,5 @@
 module HelperFunctions
 
 let undefined<'T> : 'T = failwith "Not implemented yet"
+
+let charsToString (chars: char list): string =  new string(List.toArray chars)
--- sempar/PreProcessing.fs
+++ sempar/PreProcessing.fs
@@ -1,6 +1,47 @@
 module PreProcessing
 
+open FParsec
+open HelperFunctions
+
 type Processed = P 
+
+type Code = string
+type Token = string
+type RuleCase = (Token list * Code)
+
+
+type Rule = {
+    name: string;
+    cases: RuleCase list;
+}
+
+type FSY = {
+    preamble: string;
+    rules: Rule list;
+}
 
 let preprocess (contents : string[]) : Processed =
     P 
+
+
+let pTokenDecls: Parser<string,unit> = manyCharsTill anyChar (pstring "%%")
+
+let pConstraint: Parser<string,unit> = pstring "//!" >>. restOfLine true
+
+let pRuleName: Parser<string,unit> = manyCharsTill anyChar (pchar ':') 
+
+let pRuleCaseTokens: Parser<Token list, unit> = manyTill (manyCharsTill anyChar spaces1) (pchar '{')
+
+let pRuleCaseCode: Parser<Code, unit> = manyCharsTill anyChar (pchar '}')
+
+let pRuleSingleCase: Parser<RuleCase, unit> = pRuleCaseTokens .>>. pRuleCaseCode 
+
+let pRuleCase: Parser<RuleCase, unit> = pchar '|' >>. pRuleCaseTokens .>>. pRuleCaseCode .>> newline
+
+let pRuleCases = (pRuleSingleCase |>> fun x -> [x]) <|> manyTill pRuleCase (noneOf ['|'])
+
+let pRule: Parser<Rule, unit> = pipe2 pRuleName pRuleCases (fun n c -> {name=n;cases=c})
+
+let pRules: Parser<Rule list, unit> = many pRule
+
+let pFsy: Parser<FSY, unit> = pipe2 pTokenDecls pRules (fun p rs -> {preamble=p;rules=rs})
