cdc3fa85952744d3c7d8fa181963b87619151bdd
Lucas Glimfjord <lucas.glimfjord@antura.se>
2023-02-13 09:57:56 +0100

    Changes from last week

--- sempar/PreProcessing.fs
+++ sempar/PreProcessing.fs
@@ -11,6 +11,7 @@ type RuleCase = (Token list * Code)
 
 
 type Rule = {
+    constraints: string option
     name: string;
     cases: RuleCase list;
 }
@@ -20,16 +21,37 @@ type FSY = {
     rules: Rule list;
 }
 
+let preprocess (contents : string[]) : Processed =
+    P 
+
 
 let pTokenDecls: Parser<string,unit> = manyCharsTill anyChar (pstring "%%")
 
-let pConstraint: Parser<string,unit> = pstring "//!" >>. restOfLine true
+let pRuleConstraint: Parser<string,unit> = pstring "//!" >>. restOfLine true
 
 let pRuleName: Parser<string,unit> = manyCharsTill anyChar (pchar ':') 
 
 let pRuleCaseTokens: Parser<Token list, unit> = manyTill (manyCharsTill anyChar spaces1) (pchar '{')
 
-let pRuleCaseCode: Parser<Code, unit> = manyCharsTill anyChar (pchar '}')
+let pBetweenCurlys (p: Parser<'a, unit>): Parser<'a, unit> = between (pchar '{') (pchar '}') p
+
+let pStringWithCurlys (p: Parser<string, unit>): Parser<string, unit> = pBetweenCurlys p |>> fun str -> "{" + str + "}"
+
+let pUntilCurly: Parser<string, unit> = manyCharsTill anyChar (pchar '{')
+let pAfterCurly: Parser<string, unit> = manyCharsTill anyChar (pchar '}')
+
+let rec pNestedCurlys (p: Parser<string, unit>): Parser<string, unit> = 
+    pNestedCurlys (pipe3 pUntilCurly p pAfterCurly 
+        (fun before between after -> before + "{" + between + "}" + after)
+    )
+
+// let pRuleCaseCode: Parser<Code, unit> = pNestedCurlys ( pUntilCurly .>>. pBetweenCurlys |>> 
+//     fun (before, between) -> 
+//         match String.length before with
+//         | 0 -> between
+//         | _ -> before + "{" + between
+    ) 
+// let pRuleCaseCode: Parser<Code, unit> = manyCharsTill anyChar (pchar '}' >>? spaces >>? )
 
 let pRuleSingleCase: Parser<RuleCase, unit> = pRuleCaseTokens .>>. pRuleCaseCode 
 
@@ -37,11 +59,18 @@ let pRuleCase: Parser<RuleCase, unit> = pchar '|' >>. pRuleCaseTokens .>>. pRule
 
 let pRuleCases = (pRuleSingleCase |>> fun x -> [x]) <|> manyTill pRuleCase (noneOf ['|'])
 
-let pRule: Parser<Rule, unit> = pipe2 pRuleName pRuleCases (fun n c -> {name=n;cases=c})
+let pRule: Parser<Rule, unit> = 
+    spaces >>.
+    pipe3 (pRuleConstraint |>> Some <|> preturn None) pRuleName pRuleCases 
+        (fun constraints name cases -> {constraints=constraints;name=name;cases=cases})
 
-let pRules: Parser<Rule list, unit> = many pRule
+let pComment: Parser<string, unit> = spaces >>? pstring "//" >>? notFollowedBy (pchar '!') >>. restOfLine true
 
-let pFsy: Parser<FSY, unit> = pipe2 pTokenDecls pRules (fun p rs -> {preamble=p;rules=rs})
+
+let pRules: Parser<Rule list, unit> = many ((pComment >>% None) <|> (pRule |>> Some) ) |>> List.choose id
+
+let pFsy: Parser<FSY, unit> = 
+    pipe2 pTokenDecls pRules (fun p rs -> {preamble=p;rules=rs})
 
 let preprocess (contents : string) : Processed =
     match run pFsy contents with 
