438aa57e817cb003073f83bf7cc3a92a4f9f57ea
Lucas Glimfjord <lucas.glimfjord@antura.se>
2023-03-28 10:36:48 +0200

    Outputfile changeable

--- sempar/Program.fs
+++ sempar/Program.fs
@@ -1,19 +1,38 @@
 ﻿module Program
 
+open System
 open System.IO
+open Argu
+
+type CliArguments = 
+    | [<MainCommand; ExactlyOnce; Last>] InputFile of path:string
+    | OutputFile of path:string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | InputFile _ -> "The input .sfsy file"
+            | OutputFile _ -> "Specify an output file (default: append .ppfsy to inputfilename)"
+        
 
 [<EntryPoint>]
 let main argv =
-    match argv with 
-    | argv when argv.Length <> 1 -> 
-        printf "Must provide exactly one file"
-        1
-    | [| path |] ->
-        let contents = File.ReadAllLines path |> String.concat "\n" 
-        let parseResult = contents |> Parser.parse (* |> Parser.insertConstraints *) |> (fun x -> printfn "%A" x; x.ToString())
-        File.WriteAllText(path + ".ppfsy", parseResult)
-        // printfn "%s" parseResult
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let argParser = ArgumentParser.Create<CliArguments>(errorHandler = errorHandler)
+    let arguments = argParser.Parse argv
 
-        0
+    if not (arguments.Contains InputFile) then 0 else
+
+    let inputfilePath = arguments.GetResult InputFile
+
+    let outputFilePath = match arguments.Contains OutputFile with
+                                | false -> inputfilePath + ".ppfsy"
+                                | true -> arguments.GetResult OutputFile
+
+    let contents = File.ReadAllLines inputfilePath |> String.concat "\n" 
+    let parseResult = contents |> Parser.parse (* |> Parser.insertConstraints *) |> (fun x -> printfn "%A" x; x.ToString())
+    File.WriteAllText(outputFilePath, parseResult)
+
+    0
 
         
